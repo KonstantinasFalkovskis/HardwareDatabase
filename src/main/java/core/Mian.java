5e9d835d3d2ff732dec7cbe34c6dd2823aea962a
FalcoConstantine <integro@inbox.lt>
2020-11-21 23:30:30 +0200

    Add Main.class
    - Scanner from orf.java.util.* is added to add hardware name to the database;

---
+++ src/main/java/core/Mian.java
@@ -0,0 +1,14 @@
+package core;
+
+import java.util.Scanner;
+
+public class Mian {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String hardware = scanner.nextLine();
+
+    }
+
+}
